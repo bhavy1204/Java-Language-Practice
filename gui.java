87dda9dfcfb91de83fc3869086aac2e1634039ad
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-12 10:28:40 +0530

    Started GUI apps

---
+++ gui.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class gui {
+    public static void main(String[] args) {
+        int boardWidth=600;
+        int boardHeight =boardWidth;
+
+        JFrame f = new JFrame("MEOW");
+        f.setVisible(true);
+        f.setSize(boardWidth,boardHeight);
+        f.setLocationRelativeTo(null);
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
