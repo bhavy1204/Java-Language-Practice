ee1109fd7c64647930fc3176045d86561b0b38d2
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-19 09:14:38 +0530

    Loops Question practiced

--- Basics/Loops.java
+++ Basics/Loops.java
@@ -1,4 +1,38 @@
-public class Loops {
+// public class Loops {
+//     public static void main(String args[]){
+//         for (int i=0; i<5;i++) {
+//             System.out.println("Hello");
+//             i+=2;
+//         }
+//     }
+// }
+
+
+
+// ---------------------------------------------------------------------
+
+
+// import java.util.Scanner;
+// public class Loops{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int EvenSum=0,OddSum=0;
+//         for(int i=0;i<5;i++){
+//             System.out.print("Enter a number : ");
+//             int n = sc.nextInt();
+//             if (n%2==0)
+//                 EvenSum+=n;
+//             else
+//                 OddSum+=n;
+//         }
+//         System.out.println("Even Sum  : "+ EvenSum);
+//         System.out.println("Odd Sum  : "+ OddSum);
+//     }
+// }
+
+// --------------------------------------------------------------------------
+
+public class Loops{
     public static void main(String args[]){
         
     }
