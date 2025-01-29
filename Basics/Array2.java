7d78b6c9b5c2f502f535de0428ef371ee4f1a671
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-29 10:24:13 +0530

    Added 2D arrays

---
+++ Basics/Array2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Array2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        int[][] arr= new int[row][col];
+
+        for(int i =0;i<3;i++){
+            
+        }
+
+    }
+}
--- Basics/Conditions.java
+++ Basics/Conditions.java
@@ -88,24 +88,27 @@
 
 //-----------------------------------------------------------------------------
 
-import java.util.Scanner;
 
-public class Conditions {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("ENter a year : ");
-        int year = sc.nextInt();
-        if (year < 0)
-            System.out.println("Invalid year");
-        else {
-            boolean x = year % 4 == 0;
-            boolean y = year % 100 != 0;
-            boolean z = ((year % 100 == 0) && (year % 400 == 0));
-            if (x && (y || z)) {
-                System.out.println("yes " + year + " is a leap year");
-            } else {
-                System.out.println("No " + year + " is not a leap year ");
-            }
-        }
-    }
-}
+// import java.util.Scanner;
+// public class Conditions {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("ENter a year : ");
+//         int year = sc.nextInt();
+//         if (year < 0)
+//             System.out.println("Invalid year");
+//         else {
+//             boolean x = year % 4 == 0;
+//             boolean y = year % 100 != 0;
+//             boolean z = ((year % 100 == 0) && (year % 400 == 0));
+//             if (x && (y || z))
+//                 System.out.println("yes " + year + " is a leap year");
+//             else
+//                 System.out.println("No " + year + " is not a leap year ");
+//         }
+//     }
+// }
+
+
+// ------------------------------------------------------------------------------
+
