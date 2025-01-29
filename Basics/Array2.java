0bf22edd17ddf06e412d7e65e48a7c3d71fbd21d
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-29 11:04:35 +0530

    Added 2D arrays

--- Basics/Array2.java
+++ Basics/Array2.java
@@ -1,15 +1,63 @@
 import java.util.Scanner;
 
 public class Array2 {
-    public static void main(String args[]){
+    class matrix {
         Scanner sc = new Scanner(System.in);
-        int row=sc.nextInt();
-        int col=sc.nextInt();
-        int[][] arr= new int[row][col];
+        int row, col;
+        int[][] arr;
 
-        for(int i =0;i<3;i++){
-            
+        void input() {
+            System.out.print("ENter number of rows : ");
+            this.row = sc.nextInt();
+            System.out.print("ENter number of cols : ");
+            this.col = sc.nextInt();
+            arr = new int[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    System.out.print("Enter (" + (i + 1) + "," + (j + 1) + ") index  : ");
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+
+        };
+
+        void display() {
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++)
+                    System.out.print(arr[i][j] + " ");
+                System.out.println();
+            }
         }
 
+        void add(matrix m) {
+            if (this.row == m.row && this.col == m.col) {
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < col; j++)
+                        System.out.print((this.arr[i][j] + m.arr[i][j]) + " ");
+                    System.out.println();
+                }
+            }else{
+                System.out.println("Size not same can not peform addition");
+            }
+        }
+
+        
+    };
+
+    public static void main(String args[]) {
+        Array2 obj = new Array2();
+        matrix m = obj.new matrix();
+        matrix m2 = obj.new matrix();
+
+        m.input();
+        m2.input();
+        System.out.println("Matrix 1:- ");
+        m.display();
+        System.out.println("Matrix 2:- ");
+        m2.display();
+        System.out.println("Addiition of matrix 1 and 2 ");
+        m.add(m2);
+
     }
+
 }
