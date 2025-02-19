aacad3a5b39124445779edca97a8bdfcbf6f88d5
bhavy1204 <bhavysarwa51@gmail.com>
2025-02-19 21:21:49 +0530

    Practiced questions on array

---
+++ Basics/Array/Practice1.java
@@ -0,0 +1,9 @@
+public class Practice1 {
+    public static void main (String[] args){
+        int size =10;
+        int [] temp = new int[size];   
+        for (int i : temp) {
+            System.out.println(temp[i]);
+        }
+    }
+}
