b2aa77f61d58f73bec777ab206c041440be06b7d
bhavy1204 <bhavysarwa51@gmail.com>
2025-02-22 23:52:41 +0530

    Revised functions/methods

---
+++ Functionns/basics.java
@@ -0,0 +1,14 @@
+package Functionns;
+
+public class basics {
+    public static int add(int a,int b){//parameters(formal parameters)
+        int c = a+b;
+        return c;
+    }
+    public static void main(String[] args){
+        System.out.println(add(10,20));//arguments(actual parameters)
+    }
+}
+
+
+// Call stack is made in a function call 
