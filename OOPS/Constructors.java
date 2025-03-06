df5a3c061b4af94db024e2b72a8d6f246c5a3af0
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-06 22:46:30 +0530

    Completed Swing panels

--- OOPS/Constructors.java
+++ OOPS/Constructors.java
@@ -1,16 +1,3 @@
-public class Constructors {
-    public static void main(String[] args) {
-        Demo d1 = new Demo();
-        Demo d2 = new Demo(10);
-        Demo d3 = new Demo(d2);
-        System.out.println("---------------------------------------");
-        System.out.println(d1.data);
-        System.out.println(d2.data);
-        System.out.println(d3.data);
-        // d2 = new Demo();
-    }
-}
-
 class Demo {
     int data;
 
@@ -32,3 +19,18 @@ class Demo {
         this.data = d.data;
     }
 }
+
+class Constructors {
+    public static void main(String[] args) {
+        Demo d1 = new Demo();
+        Demo d2 = new Demo(10);
+        Demo d3 = new Demo(d2);
+        System.out.println("---------------------------------------");
+        System.out.println(d1.data);
+        System.out.println(d2.data);
+        System.out.println(d3.data);
+        // d2 = new Demo();
+    }
+}
+
+
--- Swing/Panels.java
+++ Swing/Panels.java
@@ -1,17 +1,56 @@
 package Swing;
 
+// import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Panels {
     public static void main(String args[]){
+        ImageIcon img = new ImageIcon("Mylogo.jpg");
+
+        Image resizedImage = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH); // Resize to 50x50 pixels
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+
+        JLabel label = new JLabel();
+        label.setText("Hehahaha");
+        label.setIcon(resizedIcon);
+        label.setVerticalAlignment(JLabel.BOTTOM);
+        label.setVerticalTextPosition(JLabel.BOTTOM);
+        label.setBounds(50,50,100,100);
+        // pannels 
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.GREEN);
+        greenPanel.setBounds(0,0,200,200);
+        // greenPanel.setLayout(new BorderLayout());
+        greenPanel.setLayout(null);
+        greenPanel.add(label);
+
+        JPanel yellowPanel = new JPanel();
+        yellowPanel.setBackground(Color.YELLOW);
+        yellowPanel.setBounds(200,0,200,200);
+        // yellowPanel.setLayout(new BorderLayout());
+        yellowPanel.setLayout(null);
+        // yellowPanel.add(label);
+
+
+        // Creating a basic frame 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
+        frame.setLayout(null);
+        frame.setSize(400,400);
         frame.setResizable(false);
         frame.setTitle("Hehehehe hahahahahhhahaha");
         ImageIcon logo = new ImageIcon("meow.png");
         frame.setIconImage(logo.getImage());
         frame.setVisible(true);
+
+        frame.add(greenPanel);
+        frame.add(yellowPanel);
+
     }
 }
