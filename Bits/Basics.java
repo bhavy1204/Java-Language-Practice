6b7a2f936647f89684417b4cb47772018c4dc008
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-11 11:29:41 +0530

    Started bits manipulation

---
+++ Bits/Basics.java
@@ -0,0 +1,13 @@
+package Bits;
+
+public class Basics {
+    public void base(){
+        int a = 5;
+        int b=7;
+        System.out.printf(" | >>> %d ",(a | b));
+    }
+    public static void main(String[] args) {
+        Basics b = new Basics();
+        b.base();
+    }
+}
