9972e948954333ac0233a651ef5f63708a9d80d1
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-14 09:22:14 +0530

    Completed Interfaces

--- OOPS/Multiple.java
+++ OOPS/Multiple.java
@@ -1,8 +1,33 @@
+interface Engine{
+    void start();
+}
+
+interface Color{
+    String color="Black";
+    void show();
+}
 
 
+class Wheels{
+    int radius=20;
+    void showSize(){
+        System.out.println("Wheel Size : "+radius);
+    }
+}
+class Car extends Wheels implements Engine,Color{
+    public void start() {
+        System.out.println("Engine starting");
+    }
+    public void show(){
+        System.out.println("Car color : "+Color.color);
+    }
+}
 
 public class Multiple {
     public static void main(String[] args) {
-        
+        Car c = new Car();
+        c.show();
+        c.start();
+        c.showSize();
     }
 }
